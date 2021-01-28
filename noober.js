518eccd08f0a7efa40ab64193d92098e29bd2a72
mgordon14 <marissagordon61@yahoo.com>
2021-01-28 11:08:43 -0600

    First Commit

--- noober.js
+++ noober.js
@@ -6,21 +6,35 @@ function pageLoad() {
   // is refreshed or reloaded, the 'ride' variable will contain something different
   // to get started, try to write other parts of 'ride' to the JavaScript console in Chrome
   // e.g. ask 'ride' for its length or only grab the first member of the Array
-  console.log(ride)
+  // console.log(ride)
 
   // these variables map to the elements on the finished page;
   // assign the proper value to each variable
 
-  // levelOfService
-  // passenger1Name, passenger1Phone, passenger1NumberOfPassengers
-  // passenger1PickupAddressLine1, passenger1PickupAddressLine2
-  // passenger1DropoffAddressLine1, passenger1DropoffAddressLine2
-  // passenger2Name, passenger2Phone, passenger2NumberOfPassengers
-  // passenger2PickupAddressLine1, passenger2PickupAddressLine2
-  // passenger2DropoffAddressLine1, passenger2DropoffAddressLine2
-  // passenger3Name, passenger3Phone, passenger3NumberOfPassengers
-  // passenger3PickupAddressLine1, passenger3PickupAddressLine2
-  // passenger3DropoffAddressLine1, passenger3DropoffAddressLine2
+  console.log(ride)
+  levelOfService = 'Noober'
+
+  let passenger1Name, passenger1Phone, passenger1NumberOfPassengers = getRandomInt(ride.passenger1Name)
+  let passenger2Name, passenger2Phone, passenger2NumberOfPassengers = getRandomInt(ride.passenger2Name)
+  let passenger3Name, passenger3Phone, passenger3NumberOfPassengers = getRandomInt(ride.passenger3Name)
+
+  let passenger1DropoffAddressLine1  
+  
+
+
+ 
+
+
+
+  passenger1Name, passenger1Phone, passenger1NumberOfPassengers
+  passenger1PickupAddressLine1, passenger1PickupAddressLine2
+  passenger1DropoffAddressLine1, passenger1DropoffAddressLine2
+  passenger2Name, passenger2Phone, passenger2NumberOfPassengers
+  passenger2PickupAddressLine1, passenger2PickupAddressLine2
+  passenger2DropoffAddressLine1, passenger2DropoffAddressLine2
+  passenger3Name, passenger3Phone, passenger3NumberOfPassengers
+  passenger3PickupAddressLine1, passenger3PickupAddressLine2
+  passenger3DropoffAddressLine1, passenger3DropoffAddressLine2
   
   //  👆 YOUR CODE ENDS HERE. DON'T CHANGE ANY OTHER CODE 👆
 }
